33c7373b7e6781b6ae0e83ff2992d33c0ed72cb9
AdamVorley <adam_vorley@icloud.com>
2024-06-27 17:47:57 +0100

    static and template folders

--- HERAai.py
+++ HERAai.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, url_for, flash 
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+        return render_template(
+        "upload.html"
+    )
